62b2e61d7e9987a2e41c5dbd4bd84cece08868c9
Rohan Badgujar <80501166+rohanbadgujar20011@users.noreply.github.com>
2024-05-15 04:40:38 +0000

    Commit constants

--- routes/api/merchant.js
+++ routes/api/merchant.js
@@ -2,3 +2,5 @@ const express=require('express')
 const merchantRouter=express.Router();
 const bcrypt =require('bcryptjs')
 const crypto=require('crypto')
+
+const {ROLES,MERCHANT_STATUS}=require('../../constants');
